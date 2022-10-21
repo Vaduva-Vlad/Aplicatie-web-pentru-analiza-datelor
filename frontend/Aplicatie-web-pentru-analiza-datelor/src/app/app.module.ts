6e34397accf646ad66f4a144da20e8aa70d98e75
Vaduva-Vlad <vlad.vaduva02@e-uvt.ro>
2022-10-21 20:27:35 +0300

    Retrieve data from server

--- frontend/Aplicatie-web-pentru-analiza-datelor/src/app/app.module.ts
+++ frontend/Aplicatie-web-pentru-analiza-datelor/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MatSliderModule } from '@angular/material/slider';
@@ -12,6 +12,7 @@ import { MatSliderModule } from '@angular/material/slider';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     MatSliderModule
   ],
   providers: [],
