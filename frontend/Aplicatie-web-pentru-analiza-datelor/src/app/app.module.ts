72ef966aae281d588f0d0eebeaea3102572e6f41
Vaduva-Vlad <vlad.vaduva02@e-uvt.ro>
2022-10-21 21:25:39 +0300

    Add Echarts

--- frontend/Aplicatie-web-pentru-analiza-datelor/src/app/app.module.ts
+++ frontend/Aplicatie-web-pentru-analiza-datelor/src/app/app.module.ts
@@ -1,31 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-<<<<<<< HEAD
 import { NgxEchartsModule } from 'ngx-echarts';
-=======
-import { HttpClientModule } from '@angular/common/http';
->>>>>>> 6e7ec94f8e23559276f486e9b5ea216fd1db2bea
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MatSliderModule } from '@angular/material/slider';
-import { PiechartComponent } from './piechart/piechart/piechart.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PiechartComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-<<<<<<< HEAD
     NgxEchartsModule.forRoot({
       echarts: () => import('echarts')
     }),
-=======
-    HttpClientModule,
->>>>>>> 6e7ec94f8e23559276f486e9b5ea216fd1db2bea
     MatSliderModule
   ],
   providers: [],
