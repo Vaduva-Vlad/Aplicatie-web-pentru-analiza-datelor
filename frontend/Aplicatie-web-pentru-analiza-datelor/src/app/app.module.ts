a56b6023d3eb73ec1ece61aac2e3d7dfddc357a7
Vaduva-Vlad <vlad.vaduva02@e-uvt.ro>
2022-11-27 19:24:37 +0200

    Front page design

--- frontend/Aplicatie-web-pentru-analiza-datelor/src/app/app.module.ts
+++ frontend/Aplicatie-web-pentru-analiza-datelor/src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MatSliderModule } from '@angular/material/slider';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { PiechartComponent } from './piechart/piechart/piechart.component';
 import { NgxEchartsModule } from 'ngx-echarts';
 import { LinechartComponent } from './linechart/linechart.component';
+import { MatCardModule } from '@angular/material/card'
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button'
 
 @NgModule({
   declarations: [
@@ -16,12 +21,17 @@ import { LinechartComponent } from './linechart/linechart.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
     MatSliderModule,
     NgxEchartsModule.forRoot({
       echarts: () => import('echarts'),
     }),
+    MatSidenavModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
