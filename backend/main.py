831cd78cbc62f5540be33bf8eacb5b1fdb4b8fd1
Vaduva-Vlad <vlad.vaduva02@e-uvt.ro>
2022-12-29 17:03:16 +0200

    Diagrame

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,9 @@ from fastapi.middleware.cors import CORSMiddleware
 from ProcessExcel import ProcessExcel
 from models.Graph import Graph
 from models.Dashboard import Dashboard
+
+from dashboards_list import DASHBOARDS
+
 app = FastAPI()
 
 origins = [
@@ -25,42 +28,8 @@ def excelData():
 
 @app.get("/api/dashboards")
 def getDashboards():
-    option1 = {
-        "title": {
-            "text": 'Referer of a Website',
-            "subtext": 'Fake Data',
-            'left': 'center'
-        },
-        'tooltip': {
-            'trigger': 'item'
-        },
-        'legend': {
-            'orient': 'vertical',
-            'left': 'left'
-        },
-        'series': [
-            {
-                'name': 'Access From',
-                'type': 'pie',
-                'radius': '50%',
-                'data': [
-                    {'value': 1048, 'name': 'Search Engine'},
-                    {'value': 735, 'name': 'Direct'},
-                    {'value': 580, 'name': 'Email'},
-                    {'value': 484, 'name': 'Union Ads'},
-                    {'value': 300, 'name': 'Video Ads'}
-                ],
-                'emphasis': {
-                    'itemStyle': {
-                        'shadowBlur': 10,
-                        'shadowOffsetX': 0,
-                        'shadowColor': 'rgba(0, 0, 0, 0.5)'
-                    }
-                }
-            }
-        ]
-    }
-    graph1=Graph(1,option1)
-    dashboard1=Dashboard(1,'Nume Dashboard',[graph1])
-    dashboard2 = Dashboard(2, 'Nume Dashboard2', [graph1])
-    return [dashboard1,dashboard2]
+    return DASHBOARDS
+
+@app.get("/api/dashboards/{id}")
+def get_dashboard_by_id(id):
+    return DASHBOARDS[int(id)-1]
