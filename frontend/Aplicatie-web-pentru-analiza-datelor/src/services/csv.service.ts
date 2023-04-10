6e0cadac4e99378a67a221e373fc7bcf3837feca
Vaduva-Vlad <vlad.vaduva02@e-uvt.ro>
2023-04-10 16:22:13 +0300

    Added ngx-papaparse

---
+++ frontend/Aplicatie-web-pentru-analiza-datelor/src/services/csv.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Papa } from 'ngx-papaparse';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CsvService {
+
+  constructor() { }
+
+  parse(){
+  }
+}
