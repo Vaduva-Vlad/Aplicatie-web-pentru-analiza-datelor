a6028381ad41228584a227b3a46356a2ede77de5
Vaduva-Vlad <vlad.vaduva02@e-uvt.ro>
2023-06-27 17:16:55 +0300

    models

--- backend/models/User.py
+++ backend/models/User.py
@@ -1,7 +1,5 @@
-from pydantic import BaseModel
-
 class User():
-    def __init__(self,username,email,password):
-        self.username=username
-        self.email=email
-        self.password=password
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
